4922018a7206a6c571fea22187531e43f12c80af
SelfishJean123 <joanna.hornung@gmail.com>
2023-05-09 19:37:19 +0200

    add routing

--- Routing.php
+++ Routing.php
@@ -4,20 +4,26 @@ require_once 'src/controllers/DefaultController.php';
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/ProductController.php';
 
-class Routing {
+class Routing
+{
 
     public static $routes;
 
-    public static function get($url, $view) {
+    public static function get($url, $view)
+    {
         self::$routes[$url] = $view;
     }
 
-    public static function post($url, $view) {
+    public static function post($url, $view)
+    {
         self::$routes[$url] = $view;
     }
 
-    public static function run ($url) {
-        $action = explode("/", $url)[0];
+    public static function run($url)
+    {
+        $urlParts = explode("/", $url);
+        $action = $urlParts[0];
+
         if (!array_key_exists($action, self::$routes)) {
             die("Wrong url!");
         }
@@ -25,7 +31,7 @@ class Routing {
         $controller = self::$routes[$action];
         $object = new $controller;
         $action = $action ?: 'index';
-
-        $object->$action();
+        $id = $urlParts[1] ?? '';
+        $object->$action($id);
     }
 }
